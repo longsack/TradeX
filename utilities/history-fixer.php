a1f9a82b788869215bc16e60a332cc41f69fb92c
longsack <31635078+longsack@users.noreply.github.com>
2018-06-24 03:47:08 +0000

    Update history-fixer.php

--- utilities/history-fixer.php
+++ utilities/history-fixer.php
@@ -1,11 +1,8 @@
 <?php
-
 require_once 'lib/global.php';
 require_once 'utility.php';
-
 $regex_search = '~\|(\d*)(2010\d\d\d\d)\|~';
 $regex_replace = "|$1\n$2|";
-
 foreach( dir_read_files(DIR_SYSTEM) as $trade )
 {
     $file = DIR_SYSTEM_STATS . "/$trade-history";
@@ -13,7 +10,6 @@ foreach( dir_read_files(DIR_SYSTEM) as $trade )
     $history = preg_replace($regex_search, $regex_replace, $history);
     file_write($file, $history . "\n");
 }
-
 foreach( dir_read_files(DIR_TRADES) as $trade )
 {
     $file = DIR_TRADE_STATS . "/$trade-history";
@@ -21,7 +17,5 @@ foreach( dir_read_files(DIR_TRADES) as $trade )
     $history = preg_replace($regex_search, $regex_replace, $history);
     file_write($file, $history . "\n");
 }
-
 echo "DONE!\n";
-
 ?>
